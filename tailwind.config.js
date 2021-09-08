1651806597278869fd9b560d24f2257bd0157ecf
Felipe Mestre <42158804+FelipeMestre@users.noreply.github.com>
2021-09-08 00:19:52 -0300

    clase del 08/09. Instalacion de talwind y teorico sobre flex, grids, pseudoclases y pseudoelementos

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+//Permite exportar objetos y traerlos de otros lados, estamos en entorno node por eso funciona asi.
+module.exports = {
+  purge: [
+    "docs/*.html"
+  ], //Esto permite decirle a tailwind en que lugar se usa tailwind
+  darkMode: false, // or 'media' or 'class'
+  theme: {//Esto es una capa de personalizacion donde podemos agregar clases nuevas que van a estar disponibles en el html
+    extend: {
+      colors: {
+        globant: "#BFD732"
+      }
+    }, //Esto extiende lo que ya hay, si lo pones afuera pones cosas como que solo existe eso
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
+
